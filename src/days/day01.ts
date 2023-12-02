e6986da2ea99abaab429a27e32bf31c03380ea19
Jan Holeček <90050233+jan-holecek@users.noreply.github.com>
2023-12-02 20:34:48 +0100

    Add files via upload

--- src/days/day01.ts
+++ src/days/day01.ts
@@ -3,47 +3,64 @@ import { getInputByDayNumber } from "../common"
 module.exports = {
     day: "01",
     name: "Trebuchet",
+    helpers: {
+        getCalibrationValues(fileLines: string[]): number[] {
+            const calibrationValues: number[] = []
+
+            fileLines.forEach((line: string) => {
+                const lineNumbers: string[] = line.match(/\d+/g)?.join("").split("") || []
+                const calibrationValue: string = lineNumbers.length == 1 ? (lineNumbers[0] + lineNumbers[0]) : (lineNumbers[0] + lineNumbers[lineNumbers.length - 1])
+
+                calibrationValues.push(Number(calibrationValue))
+            })
+
+            return calibrationValues
+        },
+        replaceWithDigit(line: string): string {
+            const digits: { [key: string]: string } = {
+                one: "1",
+                two: "2",
+                three: "3",
+                four: "4",
+                five: "5",
+                six: "6",
+                seven: "7",
+                eight: "8",
+                nine: "9"
+            }
+
+            for (const word of Object.keys(digits).sort((a, b) => b.length - a.length)) {
+                if (line.includes(word)) {
+                    const wordRegex = new RegExp(Object.keys(digits).join('|'), 'g')
+
+                    line = line.replace(wordRegex, match => {
+                        return `${match[0]}${digits[match]}${match[match.length - 1]}`
+                    })
+                }
+            }
+            return line
+        }
+    },
     async part1(): Promise<void> {
         const fileLines: string[] = getInputByDayNumber("01").split("\n")
-        const calibrationValues: number[] = []
-
-        fileLines.forEach((line: string) => {
-            let lineNumbers: string[] = []
-            const lineArray = line.split("")
-
-            lineArray.forEach((char: string) => {
-                console.log(!isNaN(Number(char)))
-                if (!isNaN(Number(char))) lineNumbers.push(char)
-            })
-            let calibrationValue: string = lineNumbers.length == 1 ? (lineNumbers[0] + lineNumbers[0]) : (lineNumbers[0] + lineNumbers[lineNumbers.length - 1])
-
-            calibrationValues.push(Number(calibrationValue))
-        })
-        let calibrationValuesSum: number = calibrationValues.reduce((acc, current) => acc + current, 0);
+        const calibrationValues: number[] = this.helpers.getCalibrationValues(fileLines)
+        const calibrationValuesSum: number = calibrationValues.reduce((a, b) => a + b, 0)
 
         console.log(`Calibration values: ${calibrationValues}`)
         console.log(`The sum of the calibration values: ${calibrationValuesSum}`)
     },
     async part2(): Promise<void> {
-        const str = "xtwone3four"
+        const fileLines: string[] = getInputByDayNumber("01").split("\n")
+        const newFileLines: string[] = []
 
-        const numbersMap: { [key: string]: string } = {
-            "one": "1",
-            "two": "2",
-            "three": "3",
-            "four": "4",
-            "five": "5",
-            "six": "6",
-            "seven": "7",
-            "eight": "8",
-            "nine": "9",
-        }
-    
-        const newStr = Object.keys(numbersMap).reduce((acc, wordNumber) => {
-            const regExp = new RegExp(wordNumber, "g")
-            return acc.replace(regExp, numbersMap[wordNumber])
-        }, str)
-        
-        console.log(newStr)
+        fileLines.forEach((line: string) => {
+            newFileLines.push(this.helpers.replaceWithDigit(line))
+        })
+
+        const calibrationValues: number[] = this.helpers.getCalibrationValues(newFileLines)
+        const calibrationValuesSum: number = calibrationValues.reduce((a, b) => a + b, 0)
+
+        console.log(`Calibration values: ${calibrationValues}`)
+        console.log(`The sum of the calibration values: ${calibrationValuesSum}`)
     }
 }
